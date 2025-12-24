e9e5879e16e950c124b7a8e895eedcb3f835a1a9
omrajak035-svg <omrajak035@gmail.com>
2025-12-25 00:14:46 +0530

    add new change

--- calculator.py
+++ calculator.py
@@ -1,40 +1,4 @@
-print("Hello, World!")
-a = 5
-b = 3 
-sum_result = a + b 
-print("Sum of ",a," and ",b," is: ",sum_result)
-age = 18 
-old = True 
-a = None 
-fl = 6.1
-st = "om"
-print(type(old))
-print(type(a))
-print(type(age))
-print(type(st))
-print(type(fl))
-ad = 20
-ads = 5
-sum = ads%ad
-print(sum)
-#type of operators
-#aritmatic operation
-q = 7
-w = 2 
-print(q+w)
-print(q-w)
-print(q%w)
-print(q*w)
-print(q/w)
-print(q**w)
-#relation operators 
-a == 50 
-b = 20 
-print (a == b)
-print ( a!=b)
-#input 
+print = "name : om /n college slc"
 
-a = int(input ("enter your first number "))
-b = int (input ("enter your second number "))
 
-print ("sum " , a >= b )
+
