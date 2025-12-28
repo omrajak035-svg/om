67d89e5bceb3fa49bc2ac1442a97764ae50c3fb5
omrajak035-svg <omrajak035@gmail.com>
2025-12-29 03:42:02 +0530

    add now paragraph

--- loops.py
+++ loops.py
@@ -26,11 +26,20 @@
 #     i += 1
 
 
-num = [1 , 4 ,9,16,25,36,49,64,81,100]
-x = 36
-i = 0
-while i < len(num):
-    if (num [i] == x):
-        print("found it" , x)
+# num = [1 , 4 ,9,16,25,36,49,64,81,100]
+# x = 36
+# i = 0
+# while i < len(num):
+#     if (num[i] == x):
+#         print("found at index", i)
+#         break
+#     i += 1
+
+i = 1 
+while i <= 10:
+    if (i%2 == 0):
+        i += 1
+        continue
+    print(i)
     i += 1
 
