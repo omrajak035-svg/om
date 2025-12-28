424f4cc41e972588904c95b2b11e42578f30c205
omrajak035-svg <omrajak035@gmail.com>
2025-12-29 03:15:56 +0530

    add new paragraph

---
+++ loops.py
@@ -0,0 +1,36 @@
+# count = 1 
+# while count <= 6:
+#     print("Hello World")
+#     count +=1
+   
+
+# i = 1 # highest number to reverse to 
+# while i <= 3 : # 1 >= i 
+#     print(i)
+#     i += 1 # increment \ to avoid infinite loop and make sure it ends
+#     print("done")
+
+
+#     o = 1 
+#     n = int(input("enter your number"))
+#     while o <= 10:
+#         print( n*o)
+#         o += 1
+#         print("All done!")
+
+# num = [1 , 4 ,9,16,25,36,49,64,81,100]
+# heros = ["spiderman", "ironman", "thor", "hulk", "captain america"]
+# i = 0 
+# while i < len(heros):
+#     print(heros[i])
+#     i += 1
+
+
+num = [1 , 4 ,9,16,25,36,49,64,81,100]
+x = 36
+i = 0
+while i < len(num):
+    if (num [i] == x):
+        print("found it" , x)
+    i += 1
+
