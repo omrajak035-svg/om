4fc2588406244e0a29b8551dc9c20ec22dc0986d
omrajak035-svg <omrajak035@gmail.com>
2025-12-31 00:37:03 +0530

    add new file

---
+++ functionAndRecusion.py
@@ -0,0 +1,20 @@
+#function defination
+# def calc_sum(a ,b):#parameaters
+#     sum =a+b 
+#     print(sum)
+#     return sum
+
+# calc_sum( 2 , 5 )#function call ; arguments 
+
+# calc_sum( 3 , 6)
+
+# calc_sum( 2 , 6 )
+
+def print_hello():
+    print("hello")
+
+    print_hello()
+    print_hello()
+    print_hello()
+    print_hello()
+
