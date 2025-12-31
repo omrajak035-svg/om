10d23b62bd1a6b023cc274490d5c06c1d0b1eda4
omrajak035-svg <omrajak035@gmail.com>
2026-01-01 02:34:11 +0530

    add new paragraph

--- functionAndRecusion.py
+++ functionAndRecusion.py
@@ -1,20 +1,31 @@
-#function defination
-# def calc_sum(a ,b):#parameaters
-#     sum =a+b 
-#     print(sum)
-#     return sum
+# def converter(usd_val):
+#     inr_val = usd_val * 82.74
+#     print(f"INR value is: {inr_val} rs")
+#     return inr_val
 
-# calc_sum( 2 , 5 )#function call ; arguments 
+# usd = float(input("Enter USD value: "))
+# inr = converter(usd)
 
-# calc_sum( 3 , 6)
+# def show(n):
+#     if (n == 0):
+#         return
+#     print(n)
+#     show(n-1)
 
-# calc_sum( 2 , 6 )
+# show (5)
 
-def print_hello():
-    print("hello")
+# def fact(n):
+#     if (n == 1 or n == 0):
+#         return 1
+#     return fact(n-1)+n
 
-    print_hello()
-    print_hello()
-    print_hello()
-    print_hello()
+# print(" the factorial is this : " ,fact (5) )
 
+def print_list(list,idx=0):
+    if (idx == len(list)):
+        return
+    print(list[idx])
+    print_list(list , idx +1)
+
+    fruits = ["alu","mango","banana","apple","papaya"]
+    print_list(fruits)
