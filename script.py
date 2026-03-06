72eb126ccc6adab9bfc579839a04ad6c3157cf88
omrajak035-svg <omrajak035@gmail.com>
2026-03-07 00:31:46 +0530

    complete todays work

--- script.py
+++ script.py
@@ -1,1 +1,25 @@
-my_script = """welcome back to my channel,today we are diving into some massive new map leaks.make sure to suscribe for more daily updates. """
+my_script = "Welcome back! Today we are looking at the newest GTA 6 map leaks. map"
+
+words_list = my_script.split()
+total_words = len(words_list)
+
+print("SUCCESS! The code is running.")
+
+words_list = my_script.split()
+total_words = len(words_list)
+
+print("SUCCESS! The code is running.")
+print("Total words in the script:", total_words)
+
+keyword_count = my_script.count("map")
+print("Times the word'map'appears in the script: ", keyword_count)
+
+#lower 
+my_script2 = "Welcome back! Today we are looking at the newest GTA 6 map leaks."
+clean_script = my_script2.lower()
+total_word = len(clean_script)
+print ("total word in the script :",total_word)
+user_input = input("what word do ypu want to  search for ?")
+clear_word =  user_input.lower()
+keyword_count2 = clean_script.count(clear_word)
+print("times the word used :",keyword_count2)
